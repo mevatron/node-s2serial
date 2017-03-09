656581d8e9ea9d5bbfd762b0e93f1736dd5d54b1
Will Lucas <mevatron@gmail.com>
2017-03-09 12:21:18 -0600

    Added more complete function and event pass-throughs

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ function S2Serial(path, options) {
 		self.emit('open');
 	});
 
+	this._serialport.on('disconnect', function(err) {
+		self.emit('disconnect', err);
+	});
+
 	this._serialport.on('close', function () {
 		self.emit('close');
 	});
@@ -42,16 +46,52 @@ function S2Serial(path, options) {
 	this._serialport.on('error', function(err) {
 		self.emit('error', err);
 	});
-
-	this.open = this._serialport.open;
-	this.flush = this._serialport.flush;
 };
-
 util.inherits(S2Serial, Duplex);
 
 S2Serial.prototype.close = function(callback) {
 	var self = this;
-	self._seriaport.close(callback);
+	self._serialport.close(callback);
+};
+
+S2Serial.prototype.open = function(callback) {
+	var self = this;
+	self._serialport.open(callback);
+};
+
+S2Serial.prototype.flush = function(callback) {
+	var self = this;
+	self._serialport.flush(callback);
+};
+
+S2Serial.prototype.drain = function(callback) {
+	var self = this;
+	self._serialport.drain(callback);
+};
+
+S2Serial.prototype.pause = function() {
+	var self = this;
+	self._serialport.pause();
+};
+
+S2Serial.prototype.resume = function() {
+	var self = this;
+	self._serialport.resume();
+};
+
+S2Serial.prototype.set = function(options, callback) {
+	var self = this;
+	self._serialport.set(options, callback);
+};
+
+S2Serial.prototype.update = function(options, callback) {
+	var self = this;
+	self._serialport.update(options, callback);
+};
+
+S2Serial.prototype.isOpen = function() {
+	var self = this;
+	return self._serialport.isOpen();
 }
 
 module.exports = {
